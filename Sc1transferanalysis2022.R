215dad135ea41228a3b80eaddedaaa4deb81290c
Robin C. A. Barrett <rcbarret@sfu.ca>
2022-09-19 14:50:52 -0700

    Upload Public Facing Code and Data

---
+++ Sc1transferanalysis2022.R
@@ -0,0 +1,316 @@
+#
+#
+# StarTrans sc1 Lmer Analysis
+#
+# Author: Robin C. A. Barrett
+#
+#
+# Purpose: See if sc1 experience impacts sc2 performance
+# Expertise: Any_xp
+#
+# Reviewed: Joe Thompson
+# Verified: Justin O'Camb
+#
+# Inputs: So long as the "Current Data" folder containing the OnRaceOffRace_OutputTable.txt is in the working directory, this script will run on its own.
+#
+#
+# Outputs: Results for the SC1 transfer analysis. There is no reliably detectable correlation between SC1 experience and
+#   performance in the first 3 games of SC2
+#
+
+
+### libraries ####
+
+#install.packages('tidyverse')
+library(tidyverse)
+#install.packages('knitr')
+require(knitr)
+#install.packages('readr')
+library(readr)
+#install.packages('lme4')
+library(lme4)
+#install.packages('ggplot2')
+library(ggplot2)
+#install.packages('dplyr')
+library(dplyr)
+#install.packages('DT')
+library(DT)
+#install.packages('xtable')
+library(xtable)
+#install.packages('finalfit')
+library(finalfit)
+
+
+## SC1 Analysis
+# 
+# This script is intended to analyze the impact of Starcraft 1 experience on the first 3 1v1 games a player has for Starcraft 2
+# 
+# This analysis will be conducted using a simple correlational test between SC1 experience and the avergae FAL for the first 3 1v1 games played within the first 25 games of SC2 overall.
+# 
+# Grab Data:
+
+# This sets the working directory to the location of this script
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+PerformanceData <- read_csv("Current Data/OnRaceOffRace_OutputTable.txt") # path based on github location 
+
+#set PlayerID as factor
+PerformanceData$PlayerID=as.factor(PerformanceData$PlayerID)
+
+
+# Set the exclusion criteria
+DropInsufficientData=1
+DropAnyXPifover=25
+HowMany1v1Games=3
+
+
+## Apply the exclusion criteria and examine outputs
+
+UniquePlayerID=unique(PerformanceData$PlayerID)
+
+# initialize variables to store data and loop through players
+PerformanceDataUSED=data.frame()
+AnyXPforGames=data.frame()
+PerformanceDataTable = as_tibble(PerformanceData)
+numberof1v1games=data.frame()
+DroppedPlayers=data.frame(matrix(ncol = 2, nrow = 0))
+
+# Sort by Any_xp for ease of viewing individual game rows 
+PerformanceDataTable=PerformanceDataTable[order(PerformanceDataTable$Any_XP & PerformanceDataTable$Any_XP),]
+
+for(i in UniquePlayerID)
+{
+  CurrentPlayerData = filter(PerformanceDataTable, PlayerID == i)
+  CurrentPlayerDataFirst = slice_head (CurrentPlayerData, n = HowMany1v1Games)
+  
+  # Get the AnyXP of when the last game selected was played (eg. the nth 1v1 game may be the 23rd game ever played by that player)
+  maxanyXP = max(CurrentPlayerDataFirst$Any_XP)
+  
+  # Record for verification: how many xp are required to achieve enough 1v1 matches, and how many 1v1 games do they have in total.
+  AnyXPforGames=rbind(AnyXPforGames, data.frame(i, maxanyXP, nrow(CurrentPlayerDataFirst))) 
+  
+  # check to make sure the player enough 1v1 games
+  if (count(CurrentPlayerDataFirst)==HowMany1v1Games)
+  {
+    # then check if the total number of games required to reach HowMany1v1Games is less than threshold
+    if (max(CurrentPlayerDataFirst$Any_XP) <= DropAnyXPifover)
+    {
+      #if all conditions are met save the player's data
+      PerformanceDataUSED=rbind(PerformanceDataUSED, CurrentPlayerDataFirst) 
+    }
+    else
+    {
+      # If it takes too many games to reach enough 1v1 games, we drop that player
+      TempDat=t(c(i, "Too many games required to reach desired number of 1v1 matches"))
+      names(TempDat)=names(DroppedPlayers)
+      DroppedPlayers=rbind(DroppedPlayers,TempDat)
+    }
+    
+  }
+  else # If there are not enough 1v1 games in total, we drop that player
+  {
+    
+    DroppedPlayers=rbind(DroppedPlayers, c(i, "Less than enough 1v1 matches in total"))
+  }
+}
+
+PerformanceData=PerformanceDataUSED
+
+#need to drop old levels or R will try to fit them into model
+counts<-table(PerformanceData$PlayerID)
+PerformanceData$PlayerID=droplevels(PerformanceData$PlayerID, exclude=names(counts[counts==0]))
+
+# Format and Display Dropped Players and Reason for Dropping
+colnames(DroppedPlayers) <- c('PlayerID Dropped', 'Reason Dropped')
+kable(DroppedPlayers)
+
+# Now that we have the performance data of just the first 3 games for each player, we can find out how much sc1 xp they have
+
+# First get the Survey Data
+
+SurveyData <- read_csv("Current Data/BFLSurveyData.csv")
+
+
+
+##CLEANING
+
+#one level of variable was still playing sc1, so we recode that to 2013. We also set null values
+SurveyData$sc1_stop[SurveyData$sc1_stop=='Still Playing SC1']='2013'
+SurveyData$sc1_stop[SurveyData$sc1_stop=='Never Played SC1']='0'
+
+#some participants did not answer the sc1start/stop question because they did not play. Recode these as 0.
+SurveyData$sc1_stop[SurveyData$sc1_stop=='please choose' & SurveyData$sc1_experience=='No']=0
+
+#recode anyone who just did not answer question as NULL
+SurveyData$sc1_stop[SurveyData$sc1_stop=='please choose']='NULL'
+
+# Any participant who did not play SC1 can be recoded as having 0 years experience
+SurveyData$sc1_begin[SurveyData$sc1_begin=='Never Played SC1']='0'
+
+# some participants did not answer the sc1start/stop question because they did not play. Recode these as 0.
+SurveyData$sc1_begin[SurveyData$sc1_begin=='please choose' & SurveyData$sc1_experience=='No']=0
+
+# recode anyone who just did not answer question as NULL
+SurveyData$sc1_begin[SurveyData$sc1_begin=='please choose']='NULL'
+
+# check work to see if any NAs present
+unique(SurveyData$sc1_stop)
+unique(SurveyData$sc1_begin)
+
+# check for errors when converting to numeric
+summary(as.numeric(SurveyData$sc1_stop))
+summary(as.numeric(SurveyData$sc1_begin))
+
+options(warn=1)
+SurveyData$sc1_begin=as.numeric(SurveyData$sc1_begin)
+SurveyData$sc1_stop=as.numeric(SurveyData$sc1_stop)
+
+
+# drop NA values, report number of NA values
+warning(paste(sum(is.na(SurveyData$sc1_begin)==TRUE), ' NAs in begin data'))
+warning(paste(sum(is.na(SurveyData$sc1_stop)==TRUE), ' NAs in stop data'))
+SurveyData=SurveyData[is.na(SurveyData$sc1_begin)==FALSE,]
+SurveyData=SurveyData[is.na(SurveyData$sc1_stop)==FALSE,]
+
+# check work to see if any NAs remaining
+unique(SurveyData$sc1_stop)
+unique(SurveyData$sc1_begin)
+
+
+
+## Merge With Performance Data and Filter Dropped Players
+Data=merge(PerformanceData,SurveyData, by=c("PlayerID"))
+
+## Log players who do not have survey data
+PerformanceData$InSurveyData <- c(PerformanceData$PlayerID %in% SurveyData$PlayerID)
+PerformanceData[row.names(unique(PerformanceData[,c("PlayerID", "InSurveyData")])),]
+
+surveycut <- PerformanceData[PerformanceData$InSurveyData == 'FALSE', ]
+
+# Get Player Ids of missing players and attach reason for dropping
+cutplayers = data.frame(cbind(as.character(unique(surveycut$PlayerID)), rep(c("not in survey data"),each=length(unique(surveycut$PlayerID)))))
+# convert to dataframe
+names(cutplayers)<- names(DroppedPlayers)
+
+DroppedPlayers = rbind(DroppedPlayers,cutplayers)
+
+#calculate number of sc1 years 
+Data$numberofsc1years=Data$sc1_stop-Data$sc1_begin
+
+# Now that we've filtered and merged the datasets, let's look at the counts of experience levels
+
+hist(Data$numberofsc1years)
+
+
+## Now we can aggregate the data to get just one row per player.
+
+# Filter to just the columns we need
+DataForAnalysis=Data[ ,c(1,5,6,7,22)]
+
+
+# Gather UniquePlayerID that survived filtration
+UniquePlayerID=unique(DataForAnalysis$PlayerID)
+
+AggregatedDataForAnalysis = data.frame(matrix(ncol = 4, nrow = 0))
+
+# Drop empty levels
+UniquePlayerID=droplevels(UniquePlayerID)
+
+
+for(i in UniquePlayerID)
+{
+  # For each player get the playerid
+  CurrentPlayerData = filter(DataForAnalysis, PlayerID == i)
+  # Get the mean for their FALs
+  meanFAL = mean(CurrentPlayerData$falclassic)
+  # Get the mean for their average league-equivalent score
+  meanFALLeague = mean(CurrentPlayerData$Transformed_fal2League)
+  # Get the number of years reported having played StarCraft 1
+  SC1xp = unique(CurrentPlayerData$numberofsc1years)
+  
+  # Add to output matrix
+  AggregatedDataForAnalysis = rbind(AggregatedDataForAnalysis, data.frame(i, as.numeric(meanFAL), as.numeric(meanFALLeague), as.numeric(SC1xp)))
+
+}
+
+# Every player should now only have 1 row of data per person.
+# Aggregated Data can now be correlated and visualized
+names(AggregatedDataForAnalysis) <- c('PlayerID', 'avg_FAL', 'avg_LeagueTransformed', 'SC1_xp')
+AggregatedDataForAnalysis <- lapply(AggregatedDataForAnalysis,as.numeric)
+
+## First, let's look at the sumary data
+summary(AggregatedDataForAnalysis$avg_FAL)
+summary(AggregatedDataForAnalysis$avg_LeagueTransformed)
+summary(AggregatedDataForAnalysis$SC1_xp)
+
+### Assumptions Checking
+
+## Normality
+hist(AggregatedDataForAnalysis$avg_FAL)
+hist(AggregatedDataForAnalysis$avg_LeagueTransformed)
+hist(AggregatedDataForAnalysis$SC1_xp)
+
+
+## Linearity
+
+# Graph avg_FAL by SC1_xp
+plot(jitter(`avg_FAL`) ~ `SC1_xp`, data = AggregatedDataForAnalysis)
+
+# Graph LeagueTransformed by SC1_xp
+plot(jitter(`avg_LeagueTransformed`) ~ `SC1_xp`, data = AggregatedDataForAnalysis)
+
+
+# Run linear regression function so we can check assumptions
+
+# First with FAL
+SC1.avg_FAL.lm <- lm(avg_FAL ~ SC1_xp, data = AggregatedDataForAnalysis)
+
+summary(SC1.avg_FAL.lm)
+
+
+## Check for Homoscedasticity
+par(mfrow=c(2,2))
+plot(SC1.avg_FAL.lm)
+par(mfrow=c(1,1))
+
+
+# Now with League
+SC1.League.lm <- lm(avg_LeagueTransformed ~ SC1_xp, data = AggregatedDataForAnalysis)
+
+summary(SC1.League.lm)
+
+
+# Check for Homoscedasticity
+par(mfrow=c(2,2))
+plot(SC1.League.lm)
+par(mfrow=c(1,1))
+
+
+# Visualize
+FAL.graph<-ggplot(as.data.frame(AggregatedDataForAnalysis), aes(x=`SC1_xp`, y=`avg_FAL`))+
+  geom_point() + geom_smooth(method="lm", col="black") +
+  theme_bw() +
+  labs(title = "Average First Action Latency as a function of Years Played in StarCraft 1",
+       x = "StarCraft 1 Experience (Years)",
+       y = "Average First Action Latency (First 3 Games of StarCraft 2)")
+FAL.graph
+
+League.graph<-ggplot(as.data.frame(AggregatedDataForAnalysis), aes(x=`SC1_xp`, y=`avg_LeagueTransformed`))+
+  geom_point() + geom_smooth(method="lm", col="black") + 
+  theme_bw() +
+  labs(title = "Average League Equivalent as a function of Years Played in StarCraft 1",
+       x = "StarCraft 1 Experience (Years)",
+       y = "League Equivalent Score (First 3 Games of StarCraft 2)")
+League.graph
+
+# Correlation Test League by SC1
+cor.test(AggregatedDataForAnalysis$avg_LeagueTransformed, AggregatedDataForAnalysis$SC1_xp)
+
+# Correlation Test FAL by SC1
+cor.test(AggregatedDataForAnalysis$avg_FAL, AggregatedDataForAnalysis$SC1_xp)
+
+##### Neither test is significant, and so we conclude that there is not a reliably detectable corr.
+
+
+library('pwr')
+pwr.r.test(n=93, r=.5, sig.level=.05)
---
+++ WolToHotsPredictionAnalysis.R
@@ -0,0 +1,208 @@
+# Title: WolToHotsPredictionAnalysis.R
+#
+# Project: StarTransfer
+#
+# Purpose: This code creates simple linear regression models for each player, 
+#   then compares the intercept of HOTS data to the predicted intercept based on 
+#   Wings of Liberty (WOL) data.
+#
+# Author: Joe Thompson - updated Apr 25 2021
+#   Reviewed: Justin O'Camb - June 14, 2021
+#   Verified: Robin Barrett - May 24, 2021
+#   Reverified: Robin Barrett - Jun 26, 2021
+#
+# Aug 22nd change: Added power for correlation test
+#   Reviewed: Justin O'Camb 09/12/2021
+#
+# Usage: So long as the "Current Data" folder containing the wolvhos_OutputTable.txt is in the working directory, this script will run on its own.
+#
+# Notable Output: Prints graphs of each player's diagnostics and results to folder this code is saved in.
+
+#### libraries ####
+
+# install.packages('tidyverse')
+require(tidyverse)
+library(tidyr)
+library(dplyr)
+
+# install.packages('ggplot2')
+library(ggplot2)
+
+# install.packages('grid')
+library(grid)
+
+# install.packages('pwr')
+library('pwr')
+
+library(readr)
+wolVhos_OutputTable <- read_csv("Current Data/wolVhos_OutputTable.txt")
+
+
+#### Preamble ####
+
+#Training is defined as Wol games in the Version analysis and HOTS is the test
+wolVhos_OutputTable$IsTraining=as.factor(wolVhos_OutputTable$IsWoL)
+PerformanceData=wolVhos_OutputTable
+nrow(PerformanceData)
+
+
+#create container to store info on all dropped data
+Reason=as.character()
+Dropn=as.integer()
+DropStorage=data.frame(Reason,Dropn)
+
+#create containers to store extra diagnostic information
+OutputData=data.frame()
+
+
+#Prep data analysis file
+Wol_To_Hots_PredictionData=data.frame()
+
+#get unique players
+UniquePlayers=unique(PerformanceData$PlayerID)
+for (i in UniquePlayers)
+{
+  
+  #get current player data
+  Currplayer=PerformanceData[PerformanceData$PlayerID==i,]
+  
+  
+  #Insist on at least 20 WOL and HOTS
+  #Insist on at least 20 Hots games
+  GameCountCheck=sum(Currplayer$Expansion_level=='Heart of the Swarm')>20&sum(Currplayer$Expansion_level=='Wings of Liberty')>20
+  if(!GameCountCheck)
+  {
+    Dropn=nrow(Currplayer)
+    Reason=paste('player,', i, ' dropped for having too few Hots or Wol games')
+    #this player is to be dropped. Log this
+    DropStorage=rbind(DropStorage, data.frame(Reason,Dropn))
+  }
+  
+  if(GameCountCheck)
+  {
+    #Some people swap versions from HOTS back to Wol, we drop those Wol games.
+    #Find first HOTs game for this player
+    FirstHotsDate=min(Currplayer$date_time[Currplayer$IsHoS==1])
+    
+    #drop any Wol games after FirstHotsDate. Do this by dropping anything that is not a HOTS game or is something before the first hots date (i.e., WOL)
+    #log games to be dropped
+    NumberOfLateWolGames=sum(Currplayer$date_time>FirstHotsDate&Currplayer$IsWoL==1)
+    
+    if (NumberOfLateWolGames>0)
+    {
+      
+      Reason=paste('player,', i, ' has', NumberOfLateWolGames, ' late Wol games dropped')
+      Dropn=NumberOfLateWolGames
+      #this player is to be dropped. Log this
+      DropStorage=rbind(DropStorage, data.frame(Reason,Dropn))
+    }
+    
+    #Reinitialize current player by excluding any late WOL games
+    Currplayer=Currplayer[Currplayer$date_time<=FirstHotsDate|Currplayer$IsHoS,]
+    
+    #Now calculate the number of days from the last game
+    Currplayer$DaysBetweenGame = as.numeric(difftime(Currplayer$date_time, lag(Currplayer$date_time, n=1L), units = 'days'))
+    
+    
+    #Build Regression for Wol & Hots seperately
+    CurrHOTsData=Currplayer[Currplayer$Expansion_level=='Heart of the Swarm',]
+    CurrWOLsData=Currplayer[Currplayer$Expansion_level=='Wings of Liberty',]
+    
+    HOTSModel=lm(Transformed_fal2League~Any_XP, data=CurrHOTsData)
+    WOLModel=lm(Transformed_fal2League~Any_XP, data=CurrWOLsData)
+    
+    HOTSModel$coefficients
+    WOLModel$coefficients
+    
+    #Use the Any_XP data from the earliest HOTs games, and make a prediction of this using the WOL model
+    DataToPredict=CurrHOTsData[CurrHOTsData$Any_XP==min(CurrHOTsData$Any_XP),]
+    Prediction.WOL=predict.lm(object=WOLModel,DataToPredict)
+    Prediction.HOTS=predict.lm(object=HOTSModel,DataToPredict)
+    
+    #Get standard errors too.
+    PredictionSE.WOL=as.numeric(predict.lm(object=WOLModel,DataToPredict,se.fit = TRUE)[2])
+    PredictionSE.HOTs=as.numeric(predict.lm(object=HOTSModel,DataToPredict,se.fit = TRUE)[2])
+    
+    #Record output data
+    Residual=Prediction.HOTS-Prediction.WOL
+    
+    #Find number of WolGames
+    Wol_n_i=nrow(CurrWOLsData)
+    
+    #Export this information to a data frame
+    Wol_To_Hots_PredictionData=rbind(Wol_To_Hots_PredictionData, cbind(i, Prediction.WOL, Prediction.HOTS, PredictionSE.WOL, PredictionSE.HOTs, Residual, Wol_n_i))
+    
+    #Prep scatter
+    fig=ggplot(Currplayer, aes(x=Any_XP, y=Transformed_fal2League, color=Expansion_level)) +
+      geom_point() + 
+      geom_smooth(method=lm) + ylim(1,8) + geom_point(aes(size=Currplayer$DaysBetweenGame))
+    
+    #Print
+    pdf(paste('HotsFigures Player ', i, '.pdf', sep=''))
+    print(fig)
+    dev.off()
+    
+    #Print diagnostics for HOTS and WOL model
+    #Set to 4 figs per page
+    par(mfrow = c(2, 2))
+    
+    pdf(paste('HotsFigures Player ', i, 'Diagnostics_HOTS.pdf', sep=''))
+    print(plot(HOTSModel))
+    dev.off()
+    
+    pdf(paste('HotsFigures Player ', i, 'Diagnostics_WOL.pdf', sep=''))
+    print(plot(WOLModel))
+    dev.off()
+    
+    #set to 1 fig per page
+    par(mfrow = c(1, 1))
+  }
+}  
+
+hist(Wol_To_Hots_PredictionData$Prediction.WOL)
+hist(Wol_To_Hots_PredictionData$Prediction.HOTS)
+hist(Wol_To_Hots_PredictionData$Residual)
+
+
+#Do power calculations for a one sample t.test
+power.t.test(nrow(Wol_To_Hots_PredictionData), delta=1, sd(Wol_To_Hots_PredictionData$Residual), 0.05, type='one.sample')
+# Calculates a Power of 1 for d = 1.5755041, and 0.7822760 for d=0.5 which acceptable
+
+
+#Build power plot
+power.matrix <- matrix(0, nrow = 101, ncol = 2)
+colnames(power.matrix) <- c('delta','power')
+row.index <- 1
+for (D in seq(from=0, to=1, by=.01)) {
+  t.power <- power.t.test(nrow(Wol_To_Hots_PredictionData),
+                          delta=D, sd(Wol_To_Hots_PredictionData$Residual),
+                          0.05, type='one.sample')
+  
+  power.matrix[row.index,1] <- D
+  power.matrix[row.index,2] <- t.power$power
+  
+  row.index <- row.index+1
+}
+
+plot(power.matrix[,1], power.matrix[,2])
+
+
+#Perform t.test
+t.test(Wol_To_Hots_PredictionData$Residual)  
+
+#Look for a correlation between WoL skill and residual
+plot(Wol_To_Hots_PredictionData$Wol_n_i, Wol_To_Hots_PredictionData$Residual)
+plot(lm( Wol_To_Hots_PredictionData$Residual~Wol_To_Hots_PredictionData$Wol_n_i))
+
+#Diagnostics: 
+plot(Wol_To_Hots_PredictionData$Wol_n_i[c(-6,-25)], Wol_To_Hots_PredictionData$Residual[c(-6,-25)])
+plot(lm( Wol_To_Hots_PredictionData$Residual[c(-6,-25)]~Wol_To_Hots_PredictionData$Wol_n_i[c(-6,-25)]))
+
+cor.test(Wol_To_Hots_PredictionData$Wol_n_i, Wol_To_Hots_PredictionData$Residual)
+cor.test(Wol_To_Hots_PredictionData$Wol_n_i[c(-6,-25)], Wol_To_Hots_PredictionData$Residual[c(-6,-25)])
+
+#Power for correlational test
+pwr.r.test(n=32, r=.5, sig.level=0.05, alternative='two.sided')
+
+
+
